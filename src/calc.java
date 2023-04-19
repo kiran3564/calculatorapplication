8fa7ec01700c4892319059bee490b9feadf7bfc0
Srikakolapu Venkata Sai Kiran <saikiran.srikakolapu@knowledgeinfotech.com>
2023-04-19 10:24:12 +0530

    calculator code

---
+++ src/calc.java
@@ -0,0 +1,18 @@
+
+public class calc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		 int a=400;
+		 int b=200;
+		first f = new first();
+		f.addmethod(a, b);
+		f.divide(a, b);
+		f.subsctract(a, b);
+		f.multiply(a, b);
+
+	}
+
+}
---
+++ src/first.java
@@ -0,0 +1,34 @@
+
+public class first {
+
+	int a;
+	int b;
+	
+	
+		
+		 public void addmethod(int a, int b)
+		{
+			int result= a+b;
+			System.out.println("The addition of two numbers is" + result);
+			
+		}
+		 
+		 public void subsctract(int a, int b)
+		 {
+			 int result =a-b;
+			 System.out.println("The substraction of two numbers is" +result);
+	}
+		 
+		 public void multiply(int a, int b)
+		 {
+			 int result =a*b;
+			 System.out.println("The multiplcation of two numbers is" +result);
+		 }
+		 
+		 public void divide(int a, int b)
+		 {
+			 int result =a/b;
+			System.out.println("The quotient of two numbers is" +result);
+		 }
+
+}
